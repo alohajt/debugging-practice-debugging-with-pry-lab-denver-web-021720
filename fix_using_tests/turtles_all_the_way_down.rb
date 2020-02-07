df82e5c38e182304c6e606d8c779849bc63b4683
JT Lu <alohajt94@gmail.com>
2020-02-07 23:57:48 +0000

    Automatically backed up by Learn

--- fix_using_tests/turtles_all_the_way_down.rb
+++ fix_using_tests/turtles_all_the_way_down.rb
@@ -9,12 +9,15 @@ def turtles
 end
 
 def turtle_traits(turtles)
+  
+  arr = []
   turtles.map do |turtle|
     turtle[:traits].each do |trait|
-      trait
+      arr << trait
       #binding.pry
     end
   end
+  return arr
 end
 
 
